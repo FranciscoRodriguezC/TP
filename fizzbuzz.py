dd88eee0baaa2a9aa9fddf93e506c0b593d52591
Francisco Rodriguez <franciscosrc03@gmail.com>
2016-06-02 19:16:11 -0300

    adjunto estilo pep8 #1

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,14 +1,26 @@
-
-x = 0                                       #se crea un contador en 0
-while (x < 101):                            #mientras x sea menor a 101 
-    if (x % 5) == 0 and (x % 3) == 0:       #si  es multiplo de 5 y de 3
-        print x                             #imprime valor de x
-        x= x +1                             #a x se le asigna x +1
-    elif (x % 3) == 0:                      #si x es multiplo de 3
-        print "Fizz"                        #imprime Fizz
-        x = x + 1                           #a x se le asigna x +1
-    elif (x % 5) == 0:                      #si x es multiplode 5
-        print "Buzz"                        #imprime Buzz
-        x =x+1                              #a x se le asigna x +1
-    else:                                   #sino
-        x = x+ 1                            #a x se le asigna x +1
+#se crea un contador en 0
+x = 0
+#mientras x sea menor a 101
+while (x < 101):
+# si  es multiplo de 5 y de 3
+    if (x % 5) == 0 and (x % 3) == 0:
+# imprime valor de x
+        print x
+#a x se le asigna x +1
+        x = x + 1
+#si x es multiplo de 3
+    elif (x % 3) == 0:
+#imprime Fizz
+        print "Fizz"
+#a x se le asigna x +1
+        x = x + 1
+#si x es multiplode 5
+    elif (x % 5) == 0:
+#imprime Buzz
+        print "Buzz"
+#a x se le asigna x +1
+        x = x + 1
+#sino
+    else:
+#a x se le asigna x +1
+        x = x + 1
