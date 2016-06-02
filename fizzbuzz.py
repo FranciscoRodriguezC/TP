35ed57c7f457995da42fe9cc9a0febf67061b338
Francisco Rodriguez <franciscosrc03@gmail.com>
2016-06-02 19:28:44 -0300

    arreglo de planificacón y código #1

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -4,8 +4,8 @@ x = 0
 while (x < 101):
 # si  es multiplo de 5 y de 3
     if (x % 5) == 0 and (x % 3) == 0:
-# imprime valor de x
-        print x
+# imprime valor de fizzbuzz
+        print "fizzbuzz"
 #a x se le asigna x +1
         x = x + 1
 #si x es multiplo de 3
@@ -23,4 +23,5 @@ while (x < 101):
 #sino
     else:
 #a x se le asigna x +1
+        print x
         x = x + 1
