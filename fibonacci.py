e8496847496b7623486a562130dff2220dcab786
Francisco Rodriguez <franciscosrc03@gmail.com>
2016-06-06 18:19:52 -0300

    Código de fibonacci corregido con pep8

--- fibonacci.py
+++ fibonacci.py
@@ -1,21 +1,20 @@
-# fibonacci.py
-#Definicion de la funcion
+#se crea un programa llamado fibonaccise crea  funcion fib con parametro i
 def fib(i):
-# se crea una variable a = 0
-      a = 0
-# se crea una variable b = 1    
-      b = 1
-# se crea una variable c = 0   
-      c = 0
+#se crea una variable a = 0
+    a = 0
+#se crea una variable b = 1
+    b = 1
+#se crea una variable c = 0
+    c = 0
 #mientras c sea menor a i
-      while c < i:
-#imprime variable c      
-            print c, 
-# a la variable a se le asigna la variable b
+    while c < i:
+#imprime variable c
+            print c,
+#a la variable a se le asigna la variable b
             a = b
-# a la variable b se le asigna la variable c
+#a la variable b se le asigna la variable c
             b = c
-# a la variable c se le asigna la suma de a+b
+#a la variable c se le asigna la suma de a+b
             c = a+b
-#Llamamos a la funcion  ftb hasta 1000000 
-fib(1000000) 
+#Llamamos a la funcion  ftb hasta 1000000
+fib(1000000)
